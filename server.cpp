f5239ec1647160c2c8c44c57a255ee11d821ba8c
ejeasfs <3498948623@qq.com>
2024-12-21 19:11:41 +0800

    modified:   server.cpp

--- server.cpp
+++ server.cpp
@@ -1,6 +1,10 @@
 #include "framework.h"
 #include "myQQ.h"
 int serverPort = 8080;
+struct serverSession {
+    sockaddr_in addr;
+    SOCKET sock;
+};
 // “服务端”框的消息处理程序。
 INT_PTR CALLBACK Server(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -15,31 +19,30 @@ INT_PTR CALLBACK Server(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
             const WCHAR* tit = s.c_str();
             HWND title = GetDlgItem(hDlg, IDC_CLIENTtitle);
             if (title != nullptr) SetWindowText(title, tit);
-            SOCKET* sock = new SOCKET;
-            *sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-            if (*sock == NULL) {
+            serverSession* data = new serverSession;
+            data->sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+            if (data->sock == NULL) {
                 int error = WSAGetLastError();
                 return error;
             }
             int enable = 1;
-            if (setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, 
+            if (setsockopt(data->sock, SOL_SOCKET, SO_REUSEADDR,
                 reinterpret_cast<char*>(&enable),
                 sizeof(enable)) == SOCKET_ERROR)return 1;
-            sockaddr_in addr;
-            addr.sin_family = AF_INET;
-            addr.sin_addr.s_addr = INADDR_ANY; // 监听所有网络接口
-            addr.sin_port = htons(8080); // 监听8080端口
-            SetWindowLongPtr(hDlg, GWLP_USERDATA, reinterpret_cast<LPARAM>(sock));
+            data->addr.sin_family = AF_INET;
+            data->addr.sin_addr.s_addr = INADDR_ANY; // 监听所有网络接口
+            data->addr.sin_port = htons(8080); // 监听8080端口
+            SetWindowLongPtr(hDlg, GWLP_USERDATA, reinterpret_cast<LPARAM>(data));
 
             return (INT_PTR)TRUE;
         }
         case WM_COMMAND:
         {
-            SOCKET* sock = reinterpret_cast<SOCKET*>(GetWindowLongPtr(hDlg, GWLP_USERDATA));
+            serverSession* data = reinterpret_cast<serverSession*>(GetWindowLongPtr(hDlg, GWLP_USERDATA));
             if (LOWORD(wParam) == IDCANCEL)
             {
-                closesocket(*sock);
-                delete sock;
+                closesocket(data->sock);
+                delete data;
                 EndDialog(hDlg, LOWORD(wParam));
                 return (INT_PTR)TRUE;
             }
@@ -47,7 +50,8 @@ INT_PTR CALLBACK Server(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         }
         case WM_TIMER:
         {
-            SOCKET* sock = reinterpret_cast<SOCKET*>(GetWindowLongPtr(hDlg, GWLP_USERDATA));
+            serverSession* data = reinterpret_cast<serverSession*>(GetWindowLongPtr(hDlg, GWLP_USERDATA));
+
 
             break;
         }
